5d7fc848eede7a4f7c1b3ca727eda242640459c2
yusuke <y.yusuke0724@gmail.com>
2021-07-05 22:39:59 +0900

    viewファイル名修正

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from analysis.views import views
+from analysis.views import index
 from analysis.views import detail
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', index.IndexView.as_view(), name='index'),
     path('detail/', detail.DetailView.as_view(), name='detail'),
 ]
--- analysis/views/detail.py
+++ analysis/views/detail.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 from use_case.calculation import Calculation
 
 class DetailView(TemplateView):
-    template_name = 'app/detail.html'
+    template_name = 'analysis/detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
