ca8f1fa3dedd43c496b7d8ee8021c9cec5d50313
yusuke <y.yusuke0724@gmail.com>
2021-07-07 14:22:10 +0900

    単回帰分析一時コミット

--- analysis/views/detail.py
+++ analysis/views/detail.py
@@ -1,11 +1,39 @@
 from django.views.generic import TemplateView
-from use_case.calculation import Calculation
+from django.http import HttpResponse
+import pandas
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
+from use_case.calculation import Calculation
 class DetailView(TemplateView):
     template_name = 'analysis/detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['message'] = Calculation.get_hoge()
-        print(context['message'])
+        print(self.cal())
         return context
+
+    def cal(self):
+        df = pandas.read_csv('data.csv')
+        print(df)
+
+        x = df[['x']]
+        y = df[['y']]
+
+        plt.plot(x, y, 'o')
+        plt.show()
+
+        model_lr = LinearRegression()
+        model_lr.fit(x, y)
+
+        plt.plot(x, y, 'o')
+        plt.plot(x, model_lr.predict(x), linestyle="solid")
+        plt.show()
+
+        print('モデル関数の回帰変数 w1: %.3f' %model_lr.coef_)
+        print('モデル関数の切片 w2: %.3f' %model_lr.intercept_)
+        print('y= %.3fx + %.3f' % (model_lr.coef_ , model_lr.intercept_))
+        print('決定係数 R^2： ', model_lr.score(x, y))
+
+        return 'teest'
---
+++ repository/data_set.py
@@ -0,0 +1,7 @@
+from calculation.models.data_set import DataSet
+
+class DataSet:
+
+    def save(row_size, column_size):
+        data_set = DataSet(row_size, column_size)
+        data_set.save()
